047f9ddcd28a366bb2e40de4b72147d90cdddddb
Rutvik <rutvik.parmar.2103@gmail.com>
2025-08-21 13:21:43 +0530

    added styling on pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const listings = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const url = "mongodb://127.0.0.1:27017/wonderlust";
 
@@ -11,6 +12,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));  
 app.use(methodOverride('_method')); // for PUT and DELETE requests
+app.use(express.static(path.join(__dirname, '/public'))); // for serving static files
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
   res.send('Hello Wonderlust!');
